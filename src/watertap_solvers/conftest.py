7638dbaf473854672e458136dce701f8007267f1
Ludovico Bianchi <lbianchi@lbl.gov>
2024-12-09 08:46:22 -0600

    Register IDAES pytest markers

---
+++ src/watertap_solvers/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+IDAES_MARKERS = {
+    "build": "Test of model build methods",
+    "unit": "Quick tests that do not require a solver, must run in < 2 s",
+    "component": "Quick tests that may require a solver",
+    "integration": "Long duration tests",
+    "solver": "Test requires a solver",
+    "requires_idaes_solver": "Test requiring solvers distributed with IDAES",
+}
+
+
+def pytest_configure(config: pytest.Config):
+    for spec, descr in IDAES_MARKERS.items():
+        config.addinivalue_line("markers", f"{spec}: {descr}")
